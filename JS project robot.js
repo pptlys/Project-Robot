b989c2621b559507f8aeb91a55abe85f8d86cb2f
Jack YANG <pptlys@gmail.com>
2021-02-12 17:45:54 -0500

    created and debugged VillageState class

--- JS project robot.js
+++ JS project robot.js
@@ -40,3 +40,36 @@ function buildGraph(edges) {
 
 const roadGraph = buildGraph(roads);
 
+// Create class to define the state of the village
+
+class VillageState {
+    constructor (place, parcels) {
+        this.place = place;
+        this.parcels = parcels;
+    }
+
+// The move method first checks whether there is a road going from current place to the destination.
+// If not, it returns the old state since this is not a valid move.
+// The call to map function moves the parcel to the new place, and the call to filter function takes care of the delivery of the parcel.
+
+move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+        return this;
+    } else {
+        let parcels = this.parcels.map(p => {
+            if (p.place != this.place) return p;
+            return {place: destination, address: p.address};
+        }).filter(p => p.place != p.address);
+        return new VillageState(destination, parcels);
+    }
+}
+}
+
+let first = new VillageState(
+    "Post Office",
+    [{place: "Post Office", address: "Alice's House"}]
+);
+let next = first.move("Alice's House");
+
+
+
